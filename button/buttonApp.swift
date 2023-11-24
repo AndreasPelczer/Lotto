a7e3ac2e7121971d6a28b8a5fd38e56daa560b95
Andreas Pelczer <a.pelczer@gmail.com>
2023-11-24 09:43:36 +0100

    Initial Commit

---
+++ button/buttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  buttonApp.swift
+//  button
+//
+//  Created by Andreas Pelczer on 24.11.23.
+//
+
+import SwiftUI
+
+@main
+struct buttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
