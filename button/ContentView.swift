07041842899923a596d6b776913c30965a17b1f9
Andreas Pelczer <a.pelczer@gmail.com>
2023-11-24 12:47:54 +0100

    was hin

--- button/ContentView.swift
+++ button/ContentView.swift
@@ -4,21 +4,51 @@
 //
 //  Created by Andreas Pelczer on 24.11.23.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var lottoZahlen: [Int] = []
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+            Spacer()
+
+            Button(action: {
+                self.lottoZahlen = self.generatelottoZahlen()
+            }) {
+                Text("Erzeuge lottoZahlen")
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+
+            if !lottoZahlen.isEmpty {
+                Text("Lotto Zahlen: \(lottoZahlen.map { String($0) }.joined(separator: ", "))")
+                    .padding()
+            }
         }
         .padding()
     }
+
+    func generatelottoZahlen() -> [Int] {
+        var lottoZahlen = Set<Int>()
+
+        while lottoZahlen.count < 6 {
+            let zufallszahl = Int.random(in: 1...49)
+            lottoZahlen.insert(zufallszahl)
+        }
+
+        return lottoZahlen.sorted()
+    }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
